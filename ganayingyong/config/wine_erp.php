f2ae8fc5c9c9302dffc4fb9df39988382bb4b83e
zackwong <hzzzoo@126.com>
2018-02-23 10:57:23 +0800

    up

--- ganayingyong/config/wine_erp.php
+++ ganayingyong/config/wine_erp.php
@@ -1,13 +1,13 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['operators'][1] = "陈一";
+$config['operators'][1] = "郭素琴";
 $config['operators'][2] = "陈二";
 $config['operators'][3] = "陈三";
 $config['operators'][4] = "陈四";
 $config['operators'][5] = "陈五";
 
 $config['order_type'][1] = '销售';
-$config['order_type'][2] = '试酒';
+$config['order_type'][2] = '试用';
 $config['order_type'][3] = '损耗';
 $config['order_type'][4] = '退货';
 /* End of file wine_erp.php */
