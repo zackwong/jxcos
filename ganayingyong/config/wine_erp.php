2ee6cb82bf28480f9f61179e4e3e2a1aa1db31e8
zackwong <hzzzoo@126.com>
2018-02-23 15:54:30 +0800

    up

--- ganayingyong/config/wine_erp.php
+++ ganayingyong/config/wine_erp.php
@@ -1,10 +1,13 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['operators'][1] = "郭素琴";
-$config['operators'][2] = "陈二";
-$config['operators'][3] = "陈三";
-$config['operators'][4] = "陈四";
-$config['operators'][5] = "陈五";
+$config['operators'][1] = "林绮秋";
+$config['operators'][2] = "郭素琴";
+$config['operators'][3] = "洪杨辉";
+$config['operators'][4] = "江燕华";
+$config['operators'][5] = "伍凤欣";
+$config['operators'][6] = "梁爱森";
+$config['operators'][7] = "伍凤欣";
+$config['operators'][8] = "卢焯君";
 
 $config['order_type'][1] = '销售';
 $config['order_type'][2] = '试用';
